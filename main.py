1c29aa9897363a4c7c2c05dde54443ee702a6b7a
Dominik Helleberg <dominik.helleberg@inovex.de>
2021-06-11 20:29:14 +0200

    added more controls, little bit of cleanup

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import ure as re
 from server.web import WebApp, jsonify
 
 np = NeoPixel(Pin(4), 12)   # create NeoPixel driver on GPIO4 for 12 pixels
+webapp = WebApp()
 
 def do_connect():
     import network
@@ -21,20 +22,37 @@ def do_connect():
             pass
     print('network config:', wlan.ifconfig())
 
-
-print("init")
-do_connect()
-print("connected")
-webapp = WebApp()
-
+#flush the gpio and gc collect, call async via asyncio.create_task(flushLEDs())
 async def flushLEDs():
   np.write()
   gc.collect()
 
-@webapp.route('/led', method='GET')
-def get_led(request, response):
-    yield from jsonify(response, {np[0]})
+#sets all LEDS at once, returns status of all leds
+@webapp.route('/leds', method='PUT')
+def set_leds(request, response):
+    yield from request.parse_json_data()
+    led_data = request.jsondata
+    if len(led_data) == 12:
+        for i in range(len(led_data)):
+            np[i] = (led_data[i][0], led_data[i][1], led_data[i][2])
+        asyncio.create_task(flushLEDs())
+    yield from jsonify(response, list(np))
 
+
+#switch all leds off, returns status of all leds
+@webapp.route('/leds/off', method='GET')
+def set_leds_off(request, response):
+    for j in range(np.n):
+         np[j] = (0, 0, 0)
+    np.write()
+    yield from jsonify(response, list(np))
+
+#get status of all leds
+@webapp.route('/leds', method='GET')
+def get_led(request, response):
+    yield from jsonify(response, {np})
+
+#control one led
 @webapp.route((re.compile('^/led/(.+)')), method='PUT')
 def set_led(request, response):
     yield from request.parse_json_data()
@@ -43,19 +61,19 @@ def set_led(request, response):
     asyncio.create_task(flushLEDs())
     yield from jsonify(response, np[led])
 
-
+def setup():
+    print("init")
+    do_connect()
+    print("connected")
+    
 def main():
     """
     Set up the tasks and start the event loop
-    """    
+    """        
     loop = asyncio.get_event_loop()
     loop.create_task(asyncio.start_server(webapp.handle, '0.0.0.0', 80))
     gc.collect()
     loop.run_forever()
 
-
-np[0] = (255, 255, 255) # set the first pixel to white
-np.write()              # write data to all pixels
-#r, g, b = np[0]         # get first pixel colour
-
+setup()
 main()
